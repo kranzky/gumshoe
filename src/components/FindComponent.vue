7944c7edf61d9b47fd6c7a8a2b1497c4767bb2b9
Jason Hutchens <jasonhutchens@gmail.com>
2021-01-30 23:50:03 +0800

    babby steps

--- src/components/FindComponent.vue
+++ src/components/FindComponent.vue
@@ -5,7 +5,7 @@
         <h4>Somewhere?</h4>
         <p class="text-body1">Slowly arising from nothingness, you imagine yourself sprawled in a chair. But something is amiss. This place feels different, but is still enshrouded in blackness.</p>
         <p class="text-body1">You need to pull yourself together.</p>
-        <p class="text-body1">Although you feel nothing, hear nothing and smell nothing, you somehow know that those abilities are now available to you.</p>
+        <p class="text-body1">Although you <i>feel</i> nothing, <i>hear</i> nothing and <i>smell</i> nothing, you somehow know that those abilities are now available to you.</p>
       </div>
       <q-btn color="secondary" icon="construction" label="What Now?">
         <q-menu content-class="bg-primary">
@@ -14,10 +14,10 @@
               <q-item-section>Focus on your feelings</q-item-section>
             </q-item>
             <q-item clickable v-close-popup>
-              <q-item-section>Inhale deeply through your nostrils</q-item-section>
+              <q-item-section>Listen intently to the world around you</q-item-section>
             </q-item>
             <q-item clickable v-close-popup>
-              <q-item-section>Listen intently to the world around you</q-item-section>
+              <q-item-section>Inhale deeply through your nostrils</q-item-section>
             </q-item>
           </q-list>
         </q-menu>
